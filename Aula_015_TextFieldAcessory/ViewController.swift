7e8eef478fe30e143544a0c8f821e2f7bf7cecdc
Swift <swift@valter.local>
2017-02-03 21:48:23 -0200

    ok

--- Aula_015_TextFieldAcessory/ViewController.swift
+++ Aula_015_TextFieldAcessory/ViewController.swift
@@ -1,24 +1,75 @@
-//
-//  ViewController.swift
-//  Aula_015_TextFieldAcessory
-//
-//  Created by Swift on 03/02/17.
-//  Copyright © 2017 Swift. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate{
+    
+    //MARK: Outlet
+    @IBOutlet weak var minhaTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Delegate
+        minhaTextField.delegate = self
+        
+        //Instancia do método que cria os botoes na textfield
+        self.criaBotoes()
+        
+        
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: Métodos próprios
+    func criaBotoes(){
+        let barra = UIToolbar()
+        barra.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 40)
+        
+        minhaTextField.inputAccessoryView = barra
+        
+        let botaoAvancar = UIBarButtonItem(barButtonSystemItem: .fastForward, target: self, action: #selector(ViewController.clicouAvancar(_:)))
+        
+        let botaoVoltar = UIBarButtonItem(barButtonSystemItem: .rewind, target: self, action: #selector(ViewController.clicouVoltar(_:)))
+        
+        let espacamento = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let botaoDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(ViewController.clicouDone(_:)))
+        
+        let arrayBotoes = [botaoVoltar, botaoAvancar, espacamento,botaoDone]
+        
+        barra.setItems(arrayBotoes, animated: true)
     }
+    
+    func clicouAvancar(_ sender : UIBarButtonItem) {
+        print("Clicou avançar")
+    }
+    
+    func clicouVoltar(_ sender : UIBarButtonItem) {
+        print("Clicou voltar")
+    }
+    
+    func clicouDone(_ sender : UIBarButtonItem) {
+        print("Clicou done")
+    }
+    
+    
+    //MARK: Métodos de UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.becomeFirstResponder()
+        return true
+    }
+    
+    //MARK: Métodos de UIResponder
+    override var canBecomeFirstResponder: Bool{
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.becomeFirstResponder()
+    }
+    
+    //MARK: Actions prórios
+
 
 
 }
